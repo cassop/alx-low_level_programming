dcc3af9df0cd329fd4de75cbbc3957a378970a96
cassop <dis.ismusic@gmail.com>
2022-12-08 10:09:44 -0800

    fix chmod and add more files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	char hello[52];
+
+	strcpy(hello, "\"Programming is like building a multilingual puzzle");
+	puts(hello);
+	return (0);
+}
